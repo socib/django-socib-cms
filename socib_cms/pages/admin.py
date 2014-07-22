bf68d7c3500bb21e79f7605e91970e96de3c20ac
Biel Frontera <bfrontera@socib.es>
2014-07-22 14:31:02 +0200

    Improve parent filter in admin

--- socib_cms/pages/admin.py
+++ socib_cms/pages/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin import SimpleListFilter
 from django.db.models import TextField
 from ckeditor_filer.widgets import CKEditorWidget
 from mptt.admin import MPTTModelAdmin
@@ -14,13 +16,29 @@ class PageForm(FlatpageForm):
         model = models.Page
 
 
+class PageFilter(SimpleListFilter):
+    title = _('parent')
+    parameter_name = 'parent'
+
+    def lookups(self, request, model_admin):
+        pages = set([page.parent for page in model_admin.model.objects.all()])
+        return [(p.id, p.title_menu) for p in pages if p is not None]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(parent__flatpage_ptr__exact=self.value())
+        else:
+            return queryset
+
+
 class PageAdmin(MPTTModelAdmin, TranslationAdmin, FlatPageAdmin):
     form = PageForm
-    formfield_overrides = {TextField: {'widget': CKEditorWidget(config_name='default')}, }
+    formfield_overrides = {TextField: {'widget': CKEditorWidget(
+        config_name='default')}, }
     search_fields = ['title']
     filter_horizontal = ('related', 'groups',)
     list_display = ('url', 'title', 'order', 'group_list')
-    list_filter = ['parent']
+    list_filter = [PageFilter]
     list_editable = ('order',)
     fieldsets = (
         (None, {
