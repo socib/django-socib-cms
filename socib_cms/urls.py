c19f86e17bbe511b2203b20f8c4c7bc04c4cbd79
Biel Frontera <bfrontera@socib.es>
2014-09-08 14:51:40 +0200

    Save changed language in session for future visits

--- socib_cms/urls.py
+++ socib_cms/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include
 
 urlpatterns = patterns(
+    '',
     (r'^localeurl/', include('localeurl.urls')),
     (r'^ckeditor_filer/', include('ckeditor_filer.urls')),
     (r'^password/', include('password_reset.urls')),
