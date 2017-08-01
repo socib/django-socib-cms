c7ea9f39b80c8423f1c2a2e03038ac5e04b5b921
Biel Frontera <bfrontera@socib.es>
2017-08-01 16:08:24 +0200

    Replace deprecated use of mimetype arg of HttpResponse with content_type

--- socib_cms/cmsutils/views.py
+++ socib_cms/cmsutils/views.py
@@ -16,9 +16,9 @@ def proxy_to(request, path, target_url):
         mimetype = proxied_request.headers.typeheader or mimetypes.guess_type(url)
         content = proxied_request.read()
     except urllib2.HTTPError as e:
-        return HttpResponse(e.msg, status=e.code, mimetype='text/plain')
+        return HttpResponse(e.msg, status=e.code, content_type='text/plain')
     else:
-        return HttpResponse(content, status=status_code, mimetype=mimetype)
+        return HttpResponse(content, status=status_code, content_type=mimetype)
 
 
 class JSONResponseMixin(object):
