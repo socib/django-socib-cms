e5f5f137cfa4e156956c9757a7c6979e6bee51ce
Biel Frontera <bfrontera@socib.es>
2021-11-04 14:39:46 +0000

    Change dependencies to upgrade to django 1.11

--- setup.py
+++ setup.py
@@ -16,29 +16,31 @@ setup(
     include_package_data=True,
     license='MIT License',
     install_requires=(
-        'Django==1.10',
+        'Django>=1.10,<2.0',
         'Pillow',
         'easy-thumbnails',
-        'django-mptt',  # 0.6 to 0.8.6
-        'django-modeltranslation',
-        'django-password-reset',
-        'django-filer',  # 0.9.12 to 1.2.5
-        'django-taggit',  # 0.12 to 0.21.3
+        'django-mptt==0.9.1',  # 0.6 to 0.8.6
+        'django-modeltranslation==0.14.4',
+        'django-polymorphic==2.1.2',
+        'django-password-reset==1.0',
+        'django-filer==1.7.1',  # 0.9.12 to 1.2.5
+        'django-taggit==0.24.0',  # 0.12 to 0.21.3
         'djangocms-admin-style',  # 0.2.8 to 1.2.6.2
         'django-admin-shortcuts',
         'python-ldap',
-        'django-auth-ldap',
-        'django-envelope',  # 0.7 to 1.7
+        'django-auth-ldap==1.2.1',
+        'django-envelope==1.4.0',  # 0.7 to 1.7
         'django-honeypot',
-        'django-crispy-forms',
-        'django-tables2',  # 0.15.0 to 1.2.6
+        'django-crispy-forms<1.9.0',
+        'django-tables2==2.0.6',  # 0.15.0 to 1.2.6
+        'django-appconf<1.0.4',
         'django_compressor',
-        'django-ace',
+        'django-ace==1.0.3',
         # 'django-genericadmin',  # 0.6.1 < github.com/arthanson/django-genericadmin
         'elasticsearch==1.9.0',  # 2.0 is not compatible with mlt
         'haystack',  # 0.15 to 0.36
-        'django-haystack',  # 2.3.1 to 2.5.1
-        'django-ckeditor',
+        'django-haystack==2.7.0',  # 2.3.1 to 2.5.1
+        'django-ckeditor<6.0.0',
         'django-ckeditor-filebrowser-filer',
         'packaging',
         'appdirs'
--- socib_cms/__init__.py
+++ socib_cms/__init__.py
@@ -1,3 +1,4 @@
 #-*- coding: utf-8 -*-
 # version string following pep-0396 and pep-0386
-__version__ = '2.0.0'  # pragma: nocover
+__version__ = '2.1.0'  # pragma: nocover
+
